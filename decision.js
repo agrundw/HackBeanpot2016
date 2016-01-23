911459fe0ffc51054d4d72f1fec7a50c7544a197
agrundw <agrundw@gmail.com>
2016-01-23 14:29:45 -0500

    added remove functionality. TODO: randomly select and option

--- decision.js
+++ decision.js
@@ -1,9 +1,12 @@
 function add() {
-	var input = document.getElementById('input').value;
+	var input = document.getElementById('input');
 
-	var things = getThings();
-	things.push(input);
-	sessionStorage.setItem('thing', JSON.stringify(things));
+	if (input.value) {
+		var things = getThings();
+		things.push(input.value);
+		sessionStorage.setItem('thing', JSON.stringify(things));
+		input.value = '';
+	}
 
 	show();
 
@@ -12,20 +15,46 @@ function add() {
 	console.log("I was Clicked!");
 }
 
+function remove() {
+	var id = this.getAttribute('id');
+	var things = getThings();
+	things.splice(id,1);
+	sessionStorage.setItem('thing', JSON.stringify(things));
+
+	show();
+
+	return false;
+}
+
 function show() {
 	var things = getThings();
 
 	var html = '';
 	for (var i = 0; i < things.length; i++) {
-		html += genHTML(things[i]);
+		html += genHTML(things[i], i);
 	}
 
 	document.getElementById('things').innerHTML = html;
 
+	var btns = document.getElementsByClassName('remove');
+	for(var i = 0; i < btns.length; i++) {
+		btns[i].addEventListener('click', remove);
+	}
+
 }
 
-function genHTML(thing) {
-	var html ="<div class='row'><div class='col-sm-4' ></div><div id='options' class='col-sm-4 well'>"+thing+"</div><div class='col-sm-4' ></div></div>";
+function genHTML(thing, i) {
+	var html =
+	`
+	<div class="row">
+		<div class="col-sm-4" ></div>
+		<div id="options" class="col-sm-4 well">
+			${thing}
+			<button id="${i}" class="remove btn btn-danger">X</button>
+		</div>
+		<div class="col-sm-4" ></div>
+	</div>
+	`;
 
 	return html;
 }
@@ -39,7 +68,4 @@ function getThings() {
 	return things;
 }
 
-
-
 document.getElementById('add').addEventListener('click', add);
-show();
